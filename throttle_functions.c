b0f9fbea8db4b51fb780f697c576c99f3980f307
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-02-21 15:16:52 -0500

    Add licenses

--- throttle_functions.c
+++ throttle_functions.c
@@ -1,3 +1,23 @@
+/**
+* throttle_functions.c
+*
+* Copyright (C) 2017 Vincent Zvikaramba <vincent.zvikaramba@mail.utoronto.ca>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This source is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with the source code.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 #include <fcntl.h>
 #include <math.h>
 #include <sys/wait.h>
